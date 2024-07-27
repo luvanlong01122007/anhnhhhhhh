8f2952df54bc9dddd06ca71cdf7763e4341c27ba
anhnhhhhhh <vLongaccco@gmail.com>
2024-07-27 20:28:01 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -137,7 +137,6 @@ def send_tool_links(message):
 ####
 #####
 video_url = 'https://v16m-default.akamaized.net/b7650db4ac7f717b7be6bd6a04777a0d/66a418a5/video/tos/useast2a/tos-useast2a-ve-0068-euttp/o4QTIgGIrNbkAPGKKLKteXyLedLE7IEgeSzeE2/?a=0&bti=OTg7QGo5QHM6OjZALTAzYCMvcCMxNDNg&ch=0&cr=0&dr=0&lr=all&cd=0%7C0%7C0%7C0&cv=1&br=2576&bt=1288&cs=0&ds=6&ft=XE5bCqT0majPD12cy-773wUOx5EcMeF~O5&mime_type=video_mp4&qs=0&rc=Mzk1OzY7PGdpZjxkOTQ3M0Bpajh1O2w5cmlzbzMzZjgzM0AuNWJgLi02NjMxLzBgXjUyYSNzNmptMmRjazFgLS1kL2Nzcw%3D%3D&vvpl=1&l=202407261543513F37EAD38E23B6263167&btag=e00088000'
-
 @bot.message_handler(commands=['add', 'adduser'])
 def add_user(message):
     admin_id = message.from_user.id
@@ -287,7 +286,7 @@ def spam(message):
     except Exception as e:
         bot.reply_to(message, f"Lỗi Tí Rồi Chạy Lại Đê")
 
-blacklist = ["0789041631", "112", "113", "114", "115", "116", "117", "118", "119", "0", "1", "2", "3", "4", "5"]
+blacklist = ["112", "113", "114", "115", "116", "117", "118", "119", "0", "1", "2", "3", "4", "5"]
 
 @bot.message_handler(commands=['spamvip'])
 def supersms(message):
